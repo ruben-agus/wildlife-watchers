45fc0d9c0fdef23fb0cea6dc58fdc70c73f91fb8
rubander <ralexanderv89@gmail.com>
2019-07-08 16:42:03 +0200

    testing branch

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const proof = 0;
 
 const session    = require("express-session");
 const MongoStore = require('connect-mongo')(session);
