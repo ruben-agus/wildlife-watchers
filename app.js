5cf2445c307ecf4670e25abf03abfeab9bb65fe0
rubander <ralexanderv89@gmail.com>
2019-07-09 13:19:30 +0200

    database

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const flash      = require("connect-flash");
     
 
 mongoose
-  .connect('mongodb://localhost/iberian-wild-life', {useNewUrlParser: true})
+  .connect(process.env.BBDD_URL, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
