969d8c299c98bfdaacd620126d8b3b599494ae45
root <root@DESKTOP-COVV5F4.localdomain>
2019-07-09 09:16:24 +0200

    seeds, map and endpoints to try

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,37 @@
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
 
-  console.log('IronGenerator JS imported successfully!');
+//   console.log('IronGenerator JS imported successfully!');
 
-}, false);
+// }, false);
+
+
+
+let cities = {
+  madrid: {lat : 40.416775, lng : -3.703790},
+  barcelona:  {lat :41.390205, lng : 2.154007},
+  zaragoza: {lat : 41.649693, lng : -0.887712}
+}
+
+
+
+
+class City {
+  constructor(name, lat, lng) {
+      if (lat<-90 || lat> 90) throw new RangeError("You have specified a wrong lat")
+      this.name = name
+      this.lat = lat
+      this.lng = lng
+  }
+}
+
+
+const theMap = new google.maps.Map(
+  document.getElementById('map'),
+  {
+      zoom: 4,
+      center: {
+          lat: cities.madrid.lat,
+          lng: cities.madrid.lng
+      }
+  }
+);
--- routes/index.js
+++ routes/index.js
@@ -1,46 +1,128 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 const User = require("../models/User");
 const Post = require("../models/Post");
 const Animal = require("../models/Animal");
 const Comment = require("../models/Comment");
+const ensureLogin =
+  /* GET home page */
+  router.get("/", (req, res, next) => {
+    res.render("index");
+    Animal
+//   .find()
+//   .then(animal=>{
+//     res.render('animaloftheday',{animal});/////CAMBIAR
 
-/* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+//   })
+
+
+  });
+
+// router.get('/', (req, res, next) => {
+//   Animal
+//   .find()
+//   .then(animal=>{
+//     res.render('animaloftheday',{animal});
+//   }).catch((err) => {
+//     console.log(err)
+//   })
+// });
+
+// router.get('/', (req, res, next) => {
+//   Post
+//   .find()
+//   .then(post=>{
+//     res.render('index',{post});
+//   }).catch((err) => {
+//     console.log(err)
+//   })
+// });
+
+router.get("/profile", ensureLoggedIn(), (req, res, next) => {
+  res.render("profile", { user: req.user });
+});
+
+router.post("/profile-edit/:id", ensureLoggedIn(), (req, res, next) => {
+  if (req.params.id === req.user._id)
+    User.findById({ _id: req.params.id })
+      .then(user => {
+        res.render("profile", { user });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+});
+
+router.post("/postcreate", (req, res, next) => {
+  PostBlog.create({
+    title: req.body.title,
+    content: req.body.content,
+    authorId: req.user._id,
+    postImg: {
+      url: req.file.url,
+      originalName: req.file.originalname
+    }
+  })
+    .then(postNew => {
+      res.redirect("/forum");
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+router.get("/edit-post/:id", ensureLoggedIn(), (req, res, next) => {
+  Post.find({ _id: req.params.id })
+    // .populate('author')
+    .then(postDetail => {
+      res.render("edit-post", { postDetail });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+router.post("/edit-post/:id", ensureLoggedIn(), (req, res, next) => {
+  if (req.params.id === req.post.id)
+    Post.find({ _id: req.params.id })
+      // .populate('author')
+      .then(postDetail => {
+        res.render("post-detailsa", { postDetail });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+});
+router.get("/post-detail/:id", (req, res, next) => {
+  Post.findOne({ _id: req.params.id })
+    .populate("author")
+    .then(postDetail => {
+      res.render("post-details", { postDetail });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 
-router.get('/', (req, res, next) => {
-  res.render('animaloftheday');
+router.get("/forum", (req, res, next) => {
+  Post.find()
+    .then(post => {
+      res.render("forum", { post });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
-
-router.get('/', (req, res, next) => {
-  res.render('map');
-});
-
-
-router.get('/forum', (req, res, next) => {
-  res.render('forum');
-});
-
-router.get('/animalist', (req, res, next) => {
-  res.render('animallist');
-});
-
-
-router.get('/forum', (req, res, next) => {
-  res.render('/forum');
-});
-
-
-router.post("/postcreation", (req, res, next) => {
-  Post
-  .create({content:req.body.content, picPath:req.body.picPath, picName: req.body.picName})
-  res.render("auth/signup");
-});
-
-
+// router.get('/animal-list', (req, res, next) => {
+// Animal
+// .find()
+// .then(animal=>{
+//   res.render('animal-list',{animal});
+// }).catch((err) => {
+//   console.log(err)
+// })
+// });
 
 module.exports = router;
